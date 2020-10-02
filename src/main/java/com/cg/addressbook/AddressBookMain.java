a0d5c42ec66cdbd951fdcece595c6a41e0e3310f
ganirockz <ganeshrapeti8@gmail.com>
2020-10-02 18:14:23 +0530

    Welcome to Address Book System

---
+++ src/main/java/com/cg/addressbook/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.cg.addressbook;
+import java.util.*;
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book");
+	}
+
+}
