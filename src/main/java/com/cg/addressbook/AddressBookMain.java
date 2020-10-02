934eb602b0a010b96fa43d3982aa106c27f45c23
ganirockz <ganeshrapeti8@gmail.com>
2020-10-02 19:12:02 +0530

    [Ganesh]Added:Add new contact in Adress book

--- src/main/java/com/cg/addressbook/AddressBookMain.java
+++ src/main/java/com/cg/addressbook/AddressBookMain.java
@@ -1,14 +1,29 @@
 package com.cg.addressbook;
+
 import java.util.*;
-import com.cg.addressbook.service.*;
+
+import com.cg.addressbook.dto.PersonContact;
+import com.cg.addressbook.dto.*;
+import com.cg.addressbook.service.impl.*;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
 		Scanner sc = new Scanner(System.in);
-		PersonServiceImp person = new PersonServiceImp(sc);
-		person.createPerson();
-		System.out.println("successfully created");
+		System.out.println("Enter the number of persons you want to add");
+		int numberOfPersons = Integer.parseInt(sc.nextLine());
+		AddressBook book = new AddressBook();
+		PersonServiceImpl person = new PersonServiceImpl(sc);
+		while (numberOfPersons-- > 0) {
+			PersonContact contact = person.createPerson();
+			book.addPersonContact(contact);
+			System.out.println("successfully created");
+		}
+		List<PersonContact> list = book.getPersonContacts();
+		for(PersonContact p:list) {
+			person.displayPerson(p);
+		}
 	}
 
 }
